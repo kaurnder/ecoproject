c873c3321ff4a4e96aaa9234790cb52349133006
pactice-git <practice@gmail.com>
2025-07-15 13:33:38 +0530

    jhgdk

--- wp-config.php
+++ wp-config.php
@@ -29,7 +29,7 @@ define( 'DB_USER', 'localhost' );
 define( 'DB_PASSWORD', '123456' );
 
 /** Database hostname */
-define( 'DB_HOST', 'localhost' );
+define( 'DB_HOST', 'localhost');
 
 /** Database charset to use in creating database tables. */
 define( 'DB_CHARSET', 'utf8mb4' );
