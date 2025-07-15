f5c716e28efef96fa9e4228763b2cfd0be4b5bd0
pactice-git <practice@gmail.com>
2025-07-15 16:31:16 +0530

    changes

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,11 +246,6 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-echo "inderjeet kaur";
-echo "welcome";
-echo "hlo dattar kaur ji";
-echo "ki hal hai mitra";
-
 
 
 
