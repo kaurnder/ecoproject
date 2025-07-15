3cd74e7b306d5cb990e6e31170cd7aede3a9b23b
inder kaur <inder@gmail.com>
2025-07-15 15:53:20 +0530

    Hlo

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,9 +246,7 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-
-
-
+echo "inderjeet kaur";
 
 
 
