0c6475fd27f93f4621fa0ec3e59426168aa9fcf8
inder kaur <inder@gmail.com>
2025-07-15 12:08:25 +0530

    chg onfig

--- wp-config.php
+++ wp-config.php
@@ -20,7 +20,7 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'inderwpinder' );
+define( 'DB_NAME', 'inderwp' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
