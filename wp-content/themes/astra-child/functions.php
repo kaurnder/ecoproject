2ac57fe5f3ccdb091fe0705a9a5cd17297191846
pactice-git <practice@gmail.com>
2025-07-16 11:58:57 +0530

    final

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,15 +246,6 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-function display_custom_meta_boxx($post) {
-    $custom_value = get_post_meta($post->ID, '_custom_key', true); 
-    echo '<label for="custom_data">Enter Custom Data:</label>';
-    echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
-}
-
-function fun(){
-    echo "This is a fun function";
-}
 
 
 
