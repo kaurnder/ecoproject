0c9b76d467018c1701d029d95b63f99be33bfdf7
pactice-git <practice@gmail.com>
2025-07-16 12:00:47 +0530

    final

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,18 +246,3 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-
-
-
-
-
-
-
-
-
-
-
-function display_custom_meta_boxhhgx($post) {
-    echo "no conflict";
-}
-
