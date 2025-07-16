1b97fdb72c33b69d01302be9c8bd289e09943110
inder kaur <inder@gmail.com>
2025-07-16 11:07:28 +0530

    hfj

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,8 +246,9 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-
+echo "jhfjrg";
 echo "HAVE A NICE DAY";
+
 
 
 
