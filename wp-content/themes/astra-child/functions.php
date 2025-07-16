9632c82e6efe5114a7a58d8b798c4e2fa3c30db6
inder kaur <inder@gmail.com>
2025-07-16 12:56:40 +0530

    ehg

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -245,7 +245,7 @@ function display_custom_meta_box($post) {
     echo '<label for="custom_data">Enter Custom Data:</label>';
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
-echo "chg";
+
 
 
 
