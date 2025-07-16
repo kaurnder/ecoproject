91972810da0bab67c71b3d4c79adc702652c02ac
pactice-git <practice@gmail.com>
2025-07-16 11:11:13 +0530

    Great

--- wp-content/themes/astra-child/functions.php
+++ wp-content/themes/astra-child/functions.php
@@ -246,7 +246,7 @@ function display_custom_meta_box($post) {
     echo '<input type="text" id="custom_data" name="custom_data" value="' . esc_attr($custom_value) . '" />';
 }
 
-
+echo "filled";
 echo "HAVE A NICE DAY";
 echo "HAVE A GREAT DAY";
 
